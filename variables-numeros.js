3f5a47fc967823d4d0915fa7efdb4ff8343d7c96
Sofi Genta <sofiigenta@gmail.com>
2021-04-19 20:12:21 -0300

    agregando variables numeros

---
+++ variables-numeros.js
@@ -0,0 +1,27 @@
+var edad = 27
+edad += 1
+
+var peso = 75
+peso -= 2
+
+var sandwich = 1
+peso += sandwich
+
+var jugarAlFutbol = 3
+peso -=3
+
+var precioVino = 200.3
+var total = precioVino*3
+
+var total = precioVino*100*3/100
+
+//sino sabemos cuantos decimales tiene
+var total = Math.round (precioVino*100*3)/100
+//convertirlo en String
+var totalStr = total.toFixed(2)
+//pasarlo a float desde un String
+var total2 = parseFloat(totalStr)
+
+var pizza = 8
+var personas = 2
+var cantidadDePorciones = pizza/personas
